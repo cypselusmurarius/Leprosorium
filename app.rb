667392f2532401fb2c3c8c18b40c54b270f7d299
cypselusmurarius@gmail.com <cypselusmurarius@gmail.com>
2017-03-20 14:12:57 +0300

    Add textarea to /details/

--- app.rb
+++ app.rb
@@ -24,43 +24,57 @@ configure do
 end
 
 get '/' do
-	#выбираем список постов из базы данных
+	# выбираем список постов из базы данных
 
 	@results = @db.execute 'select * from Posts order by id desc'
 
 	erb :index
 end
 
-#обработчик get-запроса (браузер получает страницу с сервера)
+# обработчик get-запроса (браузер получает страницу с сервера)
 
 get '/new' do
 	erb :new
 end
 
-#обработчик post-запроса (браузер отправляет данные на сервер)
+# обработчик post-запроса (браузер отправляет данные на сервер)
 
 post '/new' do
 	content = params[:content]
 
-	#обработчик ошибки (пустое сообщение от пользователя)
+	# обработчик ошибки (пустое сообщение от пользователя)
+
 	if content.length <= 0
 		@error = 'Type post text'
 		return erb :new
 	end
 
-	#добавление(сохранение данных) в таблицу Posts нового сообщения и дату его создания
+	# добавление(сохранение данных) в таблицу Posts нового сообщения и дату его создания
+
 	@db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
 
-	#перенаправление на главную страницу
+	# перенаправление на главную страницу
+
 	redirect to '/'
 end
 
-#вывод информации о посте
+# вывод информации о посте
+
 get '/details/:post_id' do
+	# получаем переменную из url`а
+
 	post_id = params[:post_id]
 
+	# получаем список постов
+	# (у нас будет только один пост)
+
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
+
+	# выбираем этот один пост в переменную @row
+
 	@row = results[0]
+
+	# возвращаем представление details.erb
 
 	erb :details
 end
